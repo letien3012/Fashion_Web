b136d7687e351ede4c04617c487add32f9ef051f
Vo Phu Qui <145702431+phuqui0103@users.noreply.github.com>
2025-05-06 10:31:09 +0700

    Initial commit: Fashion Web Project

---
+++ Frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const message = ref("Loading...");
+onMounted(async () => {
+  const res = await fetch("http://localhost:3000/api/hello");
+  const data = await res.json();
+  message.value = data.message;
+});
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
