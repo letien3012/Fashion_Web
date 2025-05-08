22545e3e337ed3aa0fb357f236c4c1d93dd073bf
Kienb2105578 <kienb2105578@student.ctu.edu.vn>
2025-05-08 10:34:01 +0700

    Cập nhật conflit

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,14 +1,3 @@
-<script setup>
-import { ref, onMounted } from "vue";
-
-const message = ref("Loading...");
-onMounted(async () => {
-  const res = await fetch("http://localhost:3005/api/hello");
-  const data = await res.json();
-  message.value = data.message;
-});
-</script>
-
 <template>
   <div id="app">
     <router-view />
