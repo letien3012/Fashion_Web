b3e4866d5d8fbf472ff864360f11c026cea440fb
letien3012 <leminhtien3012@gmail.com>
2025-05-09 15:05:16 +0700

    change index.js

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
+import ForgotPW from "../views/ForgotPW.vue";
 const routes = [
   { path: "/", name: "Login", component: Login },
   { path: "/register", name: "Register", component: Register },
+  { path: "/forgotpw", name: "ForgotPW", component: ForgotPW },
 ];
 
 const router = createRouter({
